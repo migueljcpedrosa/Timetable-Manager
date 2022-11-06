d4625c9a0eefceb2f8a71605c36733c69a748cd7
Miguel Pedrosa <migueljcpedrosa@gmail.com>
2022-11-06 00:27:53 +0000

    Created ChangeMenu.

---
+++ ChangeMenu.cpp
@@ -0,0 +1,10 @@
+//
+// Created by m3ped on 06/11/2022.
+//
+
+#include "ChangeMenu.h"
+
+void ChangeMenu::displayChangeMenu()
+{
+
+}
---
+++ ChangeMenu.h
@@ -0,0 +1,17 @@
+//
+// Created by m3ped on 06/11/2022.
+//
+
+#ifndef UNTITLED2_CHANGEMENU_H
+#define UNTITLED2_CHANGEMENU_H
+
+#include "BaseMenu.h"
+
+
+class ChangeMenu {
+    public:
+        static void displayChangeMenu();
+};
+
+
+#endif //UNTITLED2_CHANGEMENU_H
